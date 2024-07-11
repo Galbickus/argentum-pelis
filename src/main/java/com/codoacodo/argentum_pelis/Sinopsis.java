9a0b0863bfc6a0a073d2bc5378d6a5a5dfe5ae0d
Veronica <vgalbicka@gmail.com>
2024-07-11 15:52:18 -0300

    Update Sinopsis.java

--- src/main/java/com/codoacodo/argentum_pelis/Sinopsis.java
+++ src/main/java/com/codoacodo/argentum_pelis/Sinopsis.java
@@ -1,4 +1,9 @@
 package com.codoacodo.argentum_pelis;
 
 public class Sinopsis {
+    private Long id;
+    private String titulo;
+    private String sinopsis;
+    private String personaje;
+    private String poster;
 }
