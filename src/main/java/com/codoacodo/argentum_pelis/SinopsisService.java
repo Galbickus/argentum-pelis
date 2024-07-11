3b4317a7adbabaf8fc091eebf078b0c52e31c05b
Veronica <vgalbicka@gmail.com>
2024-07-11 18:11:40 -0300

    Update SinopsisService.java

--- src/main/java/com/codoacodo/argentum_pelis/SinopsisService.java
+++ src/main/java/com/codoacodo/argentum_pelis/SinopsisService.java
@@ -1,7 +1,13 @@
 package com.codoacodo.argentum_pelis;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SinopsisService {
+    @Autowired
+    private SinopsisRepository repository;
+    public SinopsisDTO obtenerSinopsisAleatoria() {
+        Sinopsis sinopsis = repository.obtenerSinopsisAleatoria();
+        return new SinopsisDTO(sinopsis.getTitulo(),sinopsis.getFrase(),sinopsis.getPersonaje(),sinopsis.getPoster());
 }
