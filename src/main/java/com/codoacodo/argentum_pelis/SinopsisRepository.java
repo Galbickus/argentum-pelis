7b124e900432cd0f8befe3630d23bd14ffdc8e42
Veronica <vgalbicka@gmail.com>
2024-07-11 18:17:56 -0300

    agrega metodo

--- src/main/java/com/codoacodo/argentum_pelis/SinopsisRepository.java
+++ src/main/java/com/codoacodo/argentum_pelis/SinopsisRepository.java
@@ -3,4 +3,5 @@ package com.codoacodo.argentum_pelis;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SinopsisRepository extends JpaRepository<Sinopsis, Long> {
+    Sinopsis obtenerSinopsisAleatoria();
 }
