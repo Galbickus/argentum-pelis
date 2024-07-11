5d6eb5f93ce01df5cfc334beba941e880d1e2cab
Veronica <vgalbicka@gmail.com>
2024-07-11 18:12:09 -0300

    Update SinopsisController.java

--- src/main/java/com/codoacodo/argentum_pelis/SinopsisController.java
+++ src/main/java/com/codoacodo/argentum_pelis/SinopsisController.java
@@ -1,8 +1,16 @@
 package com.codoacodo.argentum_pelis;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SinopsisController {
-    
+    @Autowired
+    private SinopsisService service;
+
+    @GetMapping("/series/sinopsis")
+    public SinopsisDTO obtenerSinopsis(){
+        return service.obtenerSinopsisAleatoria();
+    }
 }
