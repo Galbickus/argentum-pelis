014e88610216dc511ac36ba8b37e013c334742a2
Veronica <vgalbicka@gmail.com>
2024-07-11 15:45:30 -0300

    Initial commit

---
+++ src/main/java/com/codoacodo/argentum_pelis/ArgentumPelisApplication.java
@@ -0,0 +1,13 @@
+package com.codoacodo.argentum_pelis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgentumPelisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArgentumPelisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codoacodo/argentum_pelis/ArgentumPelisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codoacodo.argentum_pelis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgentumPelisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
