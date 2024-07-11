d7d078b5a30db1f3651be8d5740613c003b88943
Veronica <vgalbicka@gmail.com>
2024-07-11 18:21:39 -0300

    agrega query de prueba

--- src/main/java/com/codoacodo/argentum_pelis/SinopsisRepository.java
+++ src/main/java/com/codoacodo/argentum_pelis/SinopsisRepository.java
@@ -1,7 +1,9 @@
 package com.codoacodo.argentum_pelis;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SinopsisRepository extends JpaRepository<Sinopsis, Long> {
-    Sinopsis obtenerSinopsisAleatoria();
+    @Query("SELECT s FROM Sinopsis s order by function('RANDOM') LIMIT 1")
+    public Sinopsis obtenerSinopsisAleatoria();
 }
